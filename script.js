3b8afe863430beaeac25b93b4ba6c11425998e47
roshanpaudel <paudelroshan93@gmail.com>
2025-04-16 10:25:15 +1000

    number appending function and calculator siaplay function added

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 let buttonText = null;
 let displayText = 0;
+let numberString = 0;
 
 const buttons = document.querySelectorAll("button");
 console.log(buttons);
 
-// const displayCalc = function(buttonText){
-//     document.getElementsByClassName("display")
-// }
+const displayCalc = function (numberString) {
+  const display = document.getElementById("display");
+  display.innerText = numberString;
+};
+
+const numberAppend = function (buttonText) {
+  if (Number(buttonText)) {
+    numberString += buttonText;
+    console.log(numberString);
+  } else if ((buttonText = "AC")) {
+    numberString = 0;
+  }
+};
+
 buttons.forEach((btn) => {
   btn.addEventListener("click", (event) => {
-    buttonText = event.target.textContent;
+    buttonText = event.target.innerText;
     console.log(buttonText);
-
+    numberAppend(buttonText);
+    displayCalc(numberString);
   });
 });
