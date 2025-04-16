50530fadea10fbbb0600eead91de968e2c49d3c3
roshanpaudel <paudelroshan93@gmail.com>
2025-04-16 11:13:21 +1000

    inputprocessor function added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let buttonText = null;
 let displayText = 0;
 let numberString = 0;
+let firstOperand = 0;
+let secondOperand = 0;
+let operators = ["+", "-", "/", "*", "%"];
+let operatorInput = false;
 
 const buttons = document.querySelectorAll("button");
 console.log(buttons);
@@ -10,20 +14,34 @@ const displayCalc = function (numberString) {
   display.innerText = numberString;
 };
 
-const numberAppend = function (buttonText) {
+const inputProcessor = function (buttonText) {
   if (Number(buttonText)) {
     numberString += buttonText;
     console.log(numberString);
-  } else if ((buttonText = "AC")) {
+  } else if (buttonText == "AC") {
     numberString = 0;
+    firstOperand = 0;
+    secondOperand = 0;
+    operatorInput = false;
+  } else if (buttonText == "C") {
+    numberString = 0;
+    secondOperand = 0;
+  } else if (operators.includes(buttonText)) {
+    firstOperand = numberString;
+    numberString = 0;
+    operatorInput = true;
+  } else if (Number(buttonText) && operatorInput == true) {
+    s;
   }
 };
+
+const Calculate = function (firstOperand, secondOperand) {};
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     buttonText = event.target.innerText;
     console.log(buttonText);
-    numberAppend(buttonText);
+    inputProcessor(buttonText);
     displayCalc(numberString);
   });
 });
