249016b23e06d6ccd0aa2beca3a2ef0bfb2d3d81
roshanpaudel <paudelroshan93@gmail.com>
2025-04-17 12:55:49 +1000

    inputProcessor Updated

--- script.js
+++ script.js
@@ -1,54 +1,47 @@
 let buttonText = null;
 let displayText = 0;
 let evalString = 0;
-let displayString = 0;
+let displayString = "";
 let operators = ["+", "-", "/", "*", "%"];
 let operatorInput = false;
 let result = 0;
 
+//Event-listener
 const buttons = document.querySelectorAll("button");
-console.log(buttons);
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    buttonText = event.target.innerText;
+    inputProcessor(buttonText);
+    displayCalc(displayString);
+  });
+});
 
 const displayCalc = function (displayString) {
   const display = document.getElementById("display");
   display.innerText = displayString;
 };
 
-const appendInput = function (str1, str2) {
-  if (str1 == 0) {
-    console.log(eval(str1 + str2));
-    return eval(str1 + str2);
-  } else {
-    return (str1 += str2);
-  }
+const deleteLast = function (str) {
+  str = str.splice(0, -1);
 };
 
 const inputProcessor = function (buttonText) {
   if (Number(buttonText)) {
-    displayString = appendInput(displayString, buttonText);
-    evalString = appendInput(evalString, buttonText);
-    console.log(evalString);
-  } else if (buttonText == "AC") {
-    evalString = 0;
-    displayString = 0;
-  } else if (buttonText == "C") {
-    evalString = 0;
-    displayString = 0;
+    displayString += buttonText;
+  } else if (buttonText === "AC") {
+    displayString = "";
+  } else if (buttonText === "C") {
+    deleteLast(displayString);
   } else if (operators.includes(buttonText)) {
-    evalString += buttonText;
-    displayString = 0;
-  } else if (buttonText == "=") {
-    result = eval(evalString);
-    evalString = result;
-    displayString = result;
+    if (displayString === "") {
+      displayString = "0" + buttonText;
+      return;
+    }
+    if (displayString.includes(operators)) {
+      deleteLast(displayString);
+    }
+    displayString += buttonText;
+  } else if (buttonText === "=") {
+    displayString = eval(displayString).toString();
   }
 };
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    buttonText = event.target.innerText;
-    console.log(buttonText);
-    inputProcessor(buttonText);
-    displayCalc(displayString);
-  });
-});
